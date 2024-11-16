ce5bbcfebed212f844d3d203f45ec9ad1080f105
SyunsukeHatakeyama <sdt22csc0012@edu.sca.ac.jp>
2024-11-17 05:58:19 +0900

    タイトルのモーション基礎完成

--- Assets/Scripts/Player/PlayerTitleMotioner.cs
+++ Assets/Scripts/Player/PlayerTitleMotioner.cs
@@ -14,103 +14,145 @@ public class PlayerTitleMotioner : MonoBehaviour
     private bool isMotionStarted;
 
     [SerializeField]
-    private float floatInterval;
-    [SerializeField]
     private float floatHeight;
+    [SerializeField]
+    private float hoppingHeight;
 
     private GameObject parent;
+
+    private int lastMotionNumber;
+
+    private BackGroundScroller bgs;
+    private float fallSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         parent = transform.parent.gameObject;
         motionState = MotionState.FLOATING;
+        lastMotionNumber = 0;
         isMotionStarted = false;
+        bgs = GameObject.Find("BackGroundObject").GetComponent<BackGroundScroller>();
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        switch (motionState)
+        fallSpeed = bgs.ScrollSpeed;
+        parent.transform.localPosition =
+            parent.transform.localPosition + new Vector3(0, fallSpeed) * Time.deltaTime;
+
+
+
+        if (!isMotionStarted)
         {
-            case MotionState.FLOATING:
-                if (!isMotionStarted)
-                {
+            NextMotion();
+            switch (motionState)
+            {
+                case MotionState.FLOATING:
+                    {
+                        float rand = Random.Range(1, 5);
+                        StartCoroutine(Motion_Floating(rand));
+                    }
+                    break;
 
-                }
-                break;
+                case MotionState.MOVE:
+                    {
+                        float rand = Random.Range(1, 4);
+                        StartCoroutine(Motion_Move(rand));
+                    }
+                    break;
 
-            case MotionState.HOP:
-
-                break;
+                case MotionState.HOP:
+                    {
+                        float randTime = Random.Range(0.5f, 1.0f);
+                        int randHops = Random.Range(1, 3);
+                        StartCoroutine(Motion_Hop(randHops, randTime));
+                    }
+                    break;
+            }
         }
     }
 
     private void NextMotion()
     {
-        int rand= Random.Range(0, (int)MotionState.SIZE);
+        bool isEnd = false;
+        int rand = 0;
+        while (!isEnd)
+        {
+            rand = Random.Range(0, (int)MotionState.SIZE);
+            if (rand != lastMotionNumber) isEnd = true;
+        }
         motionState = (MotionState)rand;
+        lastMotionNumber = rand;
     }
 
     private IEnumerator Motion_Floating(float time)
     {
+        isMotionStarted = true;
+
         float t = 0.0f;
         bool isEnd = false;
         while (!isEnd)
         {
-            if (t > time) isEnd = true;
-            t += Time.deltaTime;
+            if (t >= time) isEnd = true;
 
             t += Time.deltaTime;
             float x = transform.position.x;
-            float y = parent.transform.position.y + Mathf.Sin(t / floatInterval) * floatHeight;
+            float y = parent.transform.position.y + Mathf.Sin(t*Mathf.PI) * floatHeight;
             transform.position = new Vector2(x, y);
 
             yield return null;
         }
-        NextMotion();
+        transform.position = parent.transform.position;
         isMotionStarted=false;
     }
     private IEnumerator Motion_Move(float time)
     {
+        isMotionStarted = true;
+
         float t = 0.0f;
         bool isEnd = false;
-        Vector3 target=new Vector3(
-            Random.Range(-800.0f,200.0f),
-            Random.Range(-400.0f,350.0f));
+
+        Vector3 defPos = parent.transform.localPosition;
+        Vector3 target = new Vector3(
+            Random.Range(-800.0f, 200.0f),
+            Random.Range(-600.0f, 150.0f));
+        Vector3 targetVec = target - parent.transform.localPosition;
+
         StartCoroutine(Motion_Floating(time));
         while (!isEnd)
         {
-            if (t > time) isEnd = true;
+            if (t >= time) isEnd = true;
             t += Time.deltaTime;
 
-            transform.localPosition=target*t;
+            //target.y += fallSpeed * Time.deltaTime;
+            parent.transform.localPosition = defPos + targetVec * t / time;
 
             yield return null;
         }
-        NextMotion();
+        parent.transform.localPosition = target;
         isMotionStarted = false;
     }
 
     private IEnumerator Motion_Hop(int hops,float hoppingTime)
     {
+        isMotionStarted = true;
+
         float t = 0.0f;
         bool isEnd = false;
-        for (int i = 0; i < hops; i++)
+        while (!isEnd)
         {
-            while (!isEnd)
-            {
-                if (t > 0.5f) isEnd = true;
-                t += Time.deltaTime/hoppingTime;
+            if (t > 0.5f) isEnd = true;
 
-                t += Time.deltaTime;
-                float x = transform.position.x;
-                float y = parent.transform.position.y + Mathf.Abs(Mathf.Sin(t / floatInterval)) * floatHeight;
-                transform.position = new Vector2(x, y);
+            t += Time.deltaTime * hops / hoppingTime;
+            float x = transform.position.x;
+            float y = parent.transform.position.y +
+                Mathf.Abs(Mathf.Sin(t * 2 * Mathf.PI)) * hoppingHeight;
+            transform.position = new Vector2(x, y);
 
-                yield return null;
-            }
+            yield return null;
         }
-        NextMotion();
         isMotionStarted = false;
     }
 }
