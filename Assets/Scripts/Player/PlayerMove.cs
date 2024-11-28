4aeaa883082f1757278b26e6f4fdd3119b9eb9a9
Yuki <sdt22csc0018@edu.sca.ac.jp>
2024-11-28 16:28:31 +0900

    鉱石の効果研究中

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -9,18 +9,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
-    float x = 0;
-    float y = 0;
-    Vector3 dir = Vector3.zero;
     public float speed;
-    public float BoundForce;
+    public float maxSpeed;
+    public PhysicsMaterial2D physicsMaterial;
+    public float bouncinessNeutral;
+    public float bouncinessDown;
+    public float bouncinessUp;
     public GameObject PlayerSkin;
     [SerializeField] Rigidbody2D rb = new Rigidbody2D();
     public float PowerMaxTime;
     public float PowerCoolTime;
     float PowerTimer;
-    float PowerTimeStock;
-    private Vector3 moveDirection; // 移動方向
     public Image gaugeImage; // ゲージに使用するImageコンポーネント
     [Range(0, 1)] public float fillAmount = 1.0f; // 塗りつぶし率
     bool move;
@@ -32,6 +31,30 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private GameObject camera;
     private Camera cam;
     public float camSize;
+    public GameObject Back;
+    public float BackPos;
+
+    //鉱石関連
+    //Plus
+    public bool coolTimeDown; //移動クールタイム減少
+    public bool knockbackDown; //ノックバック軽減
+    public bool scoreUp; //短期間獲得スコア上昇
+    public bool visibilityUp; //視界範囲上昇
+    public bool lavaSpeedDown; //溶岩速度低下
+    //Neutral
+    public bool chargeMax; //常時チャージ移動
+    public bool destroyWithOneHit; //鉱石一撃破壊
+    public bool speedUp; //移動距離UP
+    //Minus
+    public bool lavaSpeedUp; //溶岩速度上昇
+    public bool coolTimeUp; //移動クールタイム増加
+    public bool scoreDown; //3回獲得スコア減少
+    public bool mapAmountDown; //所持可能マップ数低下
+    public bool knockbackUp; //ノックバック増加
+    public bool mapDropDown; //ピースドロップ率低下
+    public bool mapBlind; //暗黙構想ピース化
+    public bool visibilityDown; //視界範囲減少
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,6 +64,25 @@ public class PlayerMove : MonoBehaviour
         move = false;
         PowerTimer = 0;
         rb = GetComponent<Rigidbody2D>();
+        physicsMaterial.bounciness = bouncinessNeutral;
+
+        //鉱石関連
+        coolTimeDown = false;
+        knockbackDown = false;
+        scoreUp = false;
+        visibilityUp = false;
+        lavaSpeedDown = false;
+        chargeMax = false;
+        destroyWithOneHit = false;
+        speedUp = false;
+        lavaSpeedUp = false;
+        coolTimeUp = false;
+        scoreDown = false;
+        mapAmountDown = false;
+        knockbackUp = false;
+        mapDropDown = false;
+        mapBlind = false;
+        visibilityDown = false;
     }
 
     // Update is called once per frame
@@ -52,32 +94,26 @@ public class PlayerMove : MonoBehaviour
     {
         fillAmount = Mathf.Clamp01(value); // 0～1にクランプ
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {/*
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            rb.velocity = new Vector3(0, 0, 0);
-            dir = new Vector3(0, 0, 0);
-            dir += new Vector3(-x, y, 0).normalized * BoundForce * speed * Time.deltaTime;
-            x *= 0;
-            y *= 0;
-        }
-        if (collision.gameObject.CompareTag("Ceiling"))
-        {
-            rb.velocity = new Vector3(0, 0, 0);
-            dir = new Vector3(0, 0, 0);
-            dir += new Vector3(x, -y, 0).normalized * BoundForce * speed * Time.deltaTime;
-            x = 0;
-            y = 0;
-        }
-     */
-    }
 
     private void PlayerUpdate()
     {
         cam.orthographicSize = camSize;
         camera.transform.position = new Vector3(transform.position.x, transform.position.y, -10.0f);
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Back != null&&BackPos!=0)
+        {
+            Back.transform.position=new Vector3(camera.transform.position.x/BackPos,this.transform.position.y/BackPos/2,0);
+        }
+
+        // 現在の速度を取得
+        Vector2 velocity = rb.velocity;
+
+        // 速度が上限を超えた場合、制限をかける
+        if (velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = velocity.normalized * maxSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene("Title");
         }
@@ -105,13 +141,13 @@ public class PlayerMove : MonoBehaviour
         if (move)
         {
             PowerTimer -= PowerCoolTime * Time.deltaTime;
-           // gaugeImage.color = lowHealthColor;
+            gaugeImage.color = lowHealthColor;
         }
         if (PowerTimer <= 0)
         {
             PowerTimer = 0;
             move = false;
-            //gaugeImage.color = fullHealthColor;
+            gaugeImage.color = fullHealthColor;
         }
         SetGauge(PowerTimer / PowerMaxTime);
 
@@ -137,7 +173,74 @@ public class PlayerMove : MonoBehaviour
                 PlayerSkin.transform.localScale = new Vector3(-0.5f, 0.5f, 1);
             }
         }
+        //鉱石関連
+        //Plus
+        if (coolTimeDown)
+        {
 
+        }
+        if (knockbackDown)
+        {
+
+        }
+        if (scoreUp)
+        {
+
+        }
+        if (visibilityUp)
+        {
+
+        }
+        if (lavaSpeedDown)
+        {
+
+        }
+        //Neutral
+        if (chargeMax)
+        {
+
+        }
+        if (destroyWithOneHit)
+        {
+
+        }
+        if (speedUp)
+        {
+
+        }
+        //Minus
+        if (lavaSpeedUp)
+        {
+
+        }
+        if (coolTimeUp)
+        {
+
+        }
+        if (scoreDown)
+        {
+
+        }
+        if (mapAmountDown)
+        {
+
+        }
+        if (knockbackUp)
+        {
+
+        }
+        if (mapDropDown)
+        {
+
+        }
+        if (mapBlind)
+        {
+
+        }
+        if (visibilityDown)
+        {
+
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D coll)
