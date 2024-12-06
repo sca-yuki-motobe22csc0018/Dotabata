98f456fd45cadeee3ed5c2ba54a4bcbbb7dc0801
kato_kenji <sdt22csc0002@edu.sca.ac.jp>
2024-12-06 16:26:55 +0900

    ギミックオブジェクトを作成

---
+++ Assets/Scripts/Gimmick/RedRippleParent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedRippleParent : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    float timer;
+    [SerializeField] float intervalTime = 1.0f;
+
+    void Start()
+    {
+        timer = 0.0f;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0.0f)
+        {
+            Instantiate(prefab, this.gameObject.transform.position, Quaternion.identity, this.gameObject.transform);
+            timer = intervalTime;
+        }
+    }
+}
