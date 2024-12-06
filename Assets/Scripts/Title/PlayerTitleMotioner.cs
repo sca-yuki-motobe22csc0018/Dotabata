aff3ba833c5c9e8d03264491596f6c3c8e4687a7
hata <sdt22csc0012@edu.sca.ac.jp>
2024-12-06 19:00:33 +0900

    source整理途中

--- Assets/Scripts/Title/PlayerTitleMotioner.cs
+++ Assets/Scripts/Title/PlayerTitleMotioner.cs
@@ -40,11 +40,13 @@ public class PlayerTitleMotioner : MonoBehaviour
     private GameObject[] ears;
 
     [SerializeField]
-    private GameObject[] elbows;
+    private GameObject[] handParents;
 
     [SerializeField]
     private GameObject[] hands;
 
+    [SerializeField]
+    private GameObject[] wingParents;
     [SerializeField]
     private GameObject[] wings;
 
@@ -52,8 +54,13 @@ public class PlayerTitleMotioner : MonoBehaviour
     private GameObject[] tail;
 
     private float flappingTimer;
-    private float flappingRate;
+    private float flappingMagnitude;
+    private float flappingSpeed;
     private float sleptTimer;
+    [SerializeField]
+    private float sleptInterval;
+    [SerializeField]
+    private float movementHeight;
 
     // Start is called before the first frame update
     void Start()
@@ -65,14 +72,15 @@ public class PlayerTitleMotioner : MonoBehaviour
         bgs = GameObject.Find("BackGroundObject").GetComponent<BackGroundScroller>();
         fallSpeed = bgs.ScrollSpeed;
         flappingTimer = 0.0f;
-        flappingRate = 0.0f;
+        flappingMagnitude = 0.0f;
+        flappingSpeed = 0.0f;
         sleptTimer = 0.0f;
     }
 
     //Update is called once per frame
     void Update()
     {
-        Motion_Flapping(flappingRate);
+        Motion_Flapping(flappingMagnitude,flappingSpeed);
         parent.transform.localPosition =
             parent.transform.localPosition + new Vector3(0, fallSpeed) * Time.deltaTime;
         sleptTimer += Time.deltaTime;
@@ -80,7 +88,7 @@ public class PlayerTitleMotioner : MonoBehaviour
         if (!isMotionStarted)
         {
             NextMotion();
-            flappingRate = 1.0f;
+            flappingMagnitude = 1.0f;
             switch (motionState)
             {
                 case MotionState.FLOATING:
@@ -92,23 +100,42 @@ public class PlayerTitleMotioner : MonoBehaviour
 
                 case MotionState.MOVE:
                     {
-                        float rand = Random.Range(1, 4);
-                        StartCoroutine(Motion_Move(rand));
+                        const float movementTimeMin = 1.0f;
+                        const float movementTimeMax = 4.0f;
+                        float randTime = Random.Range(movementTimeMin, movementTimeMax);
+
+                        const float xMin = -800.0f;
+                        const float xMax = 200.0f;
+                        const float yMin = -600.0f;
+                        const float yMax = 150.0f;
+                        Vector3 randTarget = new Vector3(
+                            Random.Range(xMin, xMax),
+                            Random.Range(yMin, yMax));
+                        StartCoroutine(Motion_Move(randTime,randTarget));
                     }
                     break;
 
                 case MotionState.HOP:
                     {
-                        float randTime = Random.Range(0.3f, 0.75f);
-                        int randHops = Random.Range(1, 3);
+                        const int hopsMin = 1;
+                        const int hopsMax = 4;
+                        int randHops = Random.Range(hopsMin, hopsMax);
+
+                        const float hoppingTimeMin = 0.3f;
+                        const float hoppingTimeMax = 0.6f;
+                        float randTime = Random.Range(hoppingTimeMin, hoppingTimeMax);
                         StartCoroutine(Motion_Hop(randHops, randTime));
                     }
                     break;
 
                 case MotionState.SLEEP:
                     {
-                        flappingRate = 0.75f;
-                        float rand = Random.Range(3.0f, 9.0f);
+                        //眠いので羽ばたきは弱くなります
+                        flappingMagnitude = 0.75f;
+                        flappingSpeed = 0.75f;
+                        const float sleepTimeMin = 4.0f;
+                        const float sleepTImeMax = 9.0f;
+                        float rand = Random.Range(sleepTimeMin, sleepTImeMax);
                         StartCoroutine(Motion_Sleep(rand));
                     }
                     break;
@@ -116,10 +143,15 @@ public class PlayerTitleMotioner : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 次のモーションを決定する関数。
+    /// </summary>
     private void NextMotion()
     {
-        if (parent.transform.localPosition.y >= 300.0f)
+
+        if (parent.transform.localPosition.y >= movementHeight)
         {
+            //高さが規定よりも上だったら次は必ず移動モーション
             motionState = MotionState.MOVE;
             lastMotionNumber = (int)MotionState.MOVE;
             return;
@@ -127,41 +159,66 @@ public class PlayerTitleMotioner : MonoBehaviour
 
         bool isEnd = false;
         int rand = 0;
-        while (!isEnd)
+        while (!isEnd)//想定の範囲内の結果になるまで繰り返す
         {
             rand = Random.Range(0, (int)MotionState.SIZE);
+
+            //前回と同じモーションはキャンセル
             if (rand == lastMotionNumber) continue;
-            if (rand == (int)MotionState.SLEEP && sleptTimer <= 30.0f && lastMotionNumber != (int)MotionState.FLOATING) continue;
+            //跳ねるモーションは頻度を減らしたいので1/3の確率でキャンセル
             if (rand == (int)MotionState.HOP && Random.Range(0, 100) >= 33) continue;
+            //寝るモーションは前回が漂うモーション以外で、かつ規定の時間経過していなかったらキャンセル
+            if (rand == (int)MotionState.SLEEP && sleptTimer <= sleptInterval && lastMotionNumber != (int)MotionState.FLOATING) continue;
+
+            //全ての条件に引っかからなかったら終了
             isEnd = true;
         }
         motionState = (MotionState)rand;
         lastMotionNumber = rand;
     }
 
-    private void Motion_Flapping(float rate)
+    /// <summary>
+    /// 羽ばたきをする関数。
+    /// 羽ばたきをさせたい間はずっとこの関数を呼び続ける。
+    /// </summary>
+    /// <param name="speed">羽ばたきの速度に掛かる値</param>
+    /// <param name="magnitude">羽ばたきの幅に掛かる値</param>
+    private void Motion_Flapping(float magnitude,float speed)
     {
-        flappingTimer = flappingTimer > 1.0f ? flappingTimer -= 1.0f : flappingTimer += Time.deltaTime * flappingRate;
+        flappingTimer = 
+            flappingTimer > 1.0f ?                   //タイマーを1.0と比較
+            flappingTimer -= 1.0f :　　　　　　　　　//1.0より大きかったら1.0を引く
+            flappingTimer += Time.deltaTime * speed;　//1.0より小さかったらdeltaTime*speedを足す
 
+        //基本になる翼の角度を決定
         List<float> defRotZ = new List<float>
         {
             25.0f,
             -30.0f
         };
+        //羽ばたきをした時の角度を決定
         List<float> motionRotZ = new List<float>
         {
             -15.0f,
             15.0f
         };
 
-        for(int i=0;i<defRotZ.Count;i++)
+        for (int i = 0; i < defRotZ.Count; i++)
         {
+            //基本の角度に対して最大でどれくらい動かすのか計算
             float z = motionRotZ[i] - defRotZ[i];
-            wings[i].transform.GetChild(0).localRotation =
-                Quaternion.Euler(new Vector3(0, 0, defRotZ[i]) + new Vector3(0, 0, z) * (1.0f - EaseInReturn(flappingTimer)) * flappingRate);
+            //計算結果に揺動運動の時間軸を掛けて、基本の角度に足す
+            wings[i].transform.localRotation =
+                Quaternion.Euler(new Vector3(0, 0, defRotZ[i]) +
+                new Vector3(0, 0, z) * (1.0f - EaseInReturn(flappingTimer)) * magnitude);
         }
     }
 
+    /// <summary>
+    /// 寝るモーションを実行するコルーチン。
+    /// </summary>
+    /// <param name="time">睡眠時間。モーション全体の時間はこれに2.25秒足した秒数。</param>
+    /// <returns></returns>
     private IEnumerator Motion_Sleep(float time)
     {
         isMotionStarted = true;
@@ -172,9 +229,9 @@ public class PlayerTitleMotioner : MonoBehaviour
         List<GameObject[]> bodyParts = new List<GameObject[]>
         {
             ears,
-            elbows,
+            handParents,
             hands,
-            wings,
+            wingParents,
             tail
         };
         List<float> motionRotZ = new List<float>
@@ -204,6 +261,9 @@ public class PlayerTitleMotioner : MonoBehaviour
             }
         }
 
+        //頭の角度に使う変数
+        Vector3 leaning = new Vector3(0, 0, 35.0f);
+
         //こくこくするところ
         for (int n = 0; n < 2; n++)
         {
@@ -211,13 +271,15 @@ public class PlayerTitleMotioner : MonoBehaviour
             t = 0.0f;
             isEnd = false;
 
+            //モーションの幅を小さくするのに使う変数
+            float weakenRate = 0.25f;
 
             while (!isEnd)
             {
                 if (t >= 1.0f) isEnd = true;
 
                 t += Time.deltaTime;
-                transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 10.0f) * EaseInReturn(t));
+                transform.localRotation = Quaternion.Euler(leaning * weakenRate * EaseInReturn(t));
                 eyes[0].transform.localScale = new Vector3(defScale.x, defScale.y * (1 - EaseInReturn(t)));
                 eyes[1].transform.localScale = new Vector3(defScale.x, defScale.y * (1 - EaseInReturn(t)));
 
@@ -227,7 +289,7 @@ public class PlayerTitleMotioner : MonoBehaviour
                     for (int j = 0; j < bodyParts[i].Length; j++)
                     {
                         bodyParts[i][j].transform.localRotation =
-                            Quaternion.Euler(new Vector3(0, 0, motionRotZ[count] * 0.25f) * EaseInReturn(t));
+                            Quaternion.Euler(new Vector3(0, 0, motionRotZ[count] * weakenRate) * EaseInReturn(t));
                         count++;
                     }
                 }
@@ -255,7 +317,7 @@ public class PlayerTitleMotioner : MonoBehaviour
             if (t >= 1.0f) isEnd = true;
 
             t += Time.deltaTime;
-            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 35.0f) * EaseInExpo(t));
+            transform.localRotation = Quaternion.Euler(leaning * EaseInExpo(t));
 
             eyes[0].transform.localScale = new Vector3(defScale.x, defScale.y * (1 - EaseInExpo(t)));
             eyes[1].transform.localScale = new Vector3(defScale.x, defScale.y * (1 - EaseInExpo(t)));
@@ -274,7 +336,7 @@ public class PlayerTitleMotioner : MonoBehaviour
             yield return null;
         }
 
-        //寝てから落ちて行くところ
+        //寝てから落ちて行くモーション
         t = 0.0f;
         isEnd = false;
 
@@ -285,7 +347,10 @@ public class PlayerTitleMotioner : MonoBehaviour
 
             parent.transform.localScale = new Vector3(1.0f + t / 3, 1.0f - t / 2, 1);
             fallSpeed = -150.0f * t;
-            flappingRate = 0.5f - (0.4f * t);
+            //magnitudeとspeedに使いたい値が同じ
+            float ft = 0.5f - (0.4f * t);
+            flappingMagnitude = ft;
+            flappingSpeed = ft;
             yield return null;
         }
         fallSpeed = bgs.ScrollSpeed;
@@ -294,6 +359,7 @@ public class PlayerTitleMotioner : MonoBehaviour
         t = 0.0f;
         isEnd = false;
 
+        //飛び起きた時の短いジャンプ
         StartCoroutine(Motion_Hop(1, 0.2f));
         eyes[0].transform.localScale = new Vector3(defScale.x, defScale.y);
         eyes[1].transform.localScale = new Vector3(defScale.x, defScale.y);
@@ -302,7 +368,7 @@ public class PlayerTitleMotioner : MonoBehaviour
             if (t >= 1.0f) isEnd = true;
             t += Time.deltaTime / 0.25f;
 
-            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 35.0f) * (1.0f - EaseInOutBack(t)));
+            transform.localRotation = Quaternion.Euler(leaning * (1.0f - EaseInOutBack(t)));
             int count = 0;
             for (int i = 0; i < bodyParts.Count; i++)
             {
@@ -318,6 +384,12 @@ public class PlayerTitleMotioner : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         sleptTimer = 0.0f;
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="time"></param>
+    /// <returns></returns>
     private IEnumerator Motion_Floating(float time)
     {
         isMotionStarted = true;
@@ -338,7 +410,13 @@ public class PlayerTitleMotioner : MonoBehaviour
         transform.position = parent.transform.position;
         isMotionStarted = false;
     }
-    private IEnumerator Motion_Move(float time)
+
+    /// <summary>
+    /// 移動のモーションを実行するコルーチン
+    /// </summary>
+    /// <param name="time">移動にかかる時間</param>
+    /// <returns></returns>
+    private IEnumerator Motion_Move(float time,Vector3 movePos)
     {
         isMotionStarted = true;
 
@@ -346,10 +424,7 @@ public class PlayerTitleMotioner : MonoBehaviour
         bool isEnd = false;
 
         Vector3 defPos = parent.transform.localPosition;
-        Vector3 target = new Vector3(
-            Random.Range(-800.0f, 200.0f),
-            Random.Range(-600.0f, 150.0f));
-        Vector3 targetVec = target - parent.transform.localPosition;
+        Vector3 targetVec = movePos - parent.transform.localPosition;
 
         if (targetVec.x < 0) parent.transform.rotation = directionLeft;
         else parent.transform.rotation = direntionRight;
@@ -364,11 +439,17 @@ public class PlayerTitleMotioner : MonoBehaviour
 
             yield return null;
         }
-        parent.transform.localPosition = target;
+        parent.transform.localPosition = movePos;
         isMotionStarted = false;
     }
 
-    private IEnumerator Motion_Hop(int hops, float hoppingTime)
+    /// <summary>
+    /// 跳ねるモーションを実行するコルーチン。モーションの全体時間はhops*time+0.1秒。
+    /// </summary>
+    /// <param name="hops">跳ねる回数</param>
+    /// <param name="time">一回の跳ねに掛かる時間</param>
+    /// <returns></returns>
+    private IEnumerator Motion_Hop(int hops, float time)
     {
         isMotionStarted = true;
 
@@ -399,7 +480,7 @@ public class PlayerTitleMotioner : MonoBehaviour
                 if (t > 1.0f) isEnd = true;
 
                 parent.transform.localScale = defScale + motionScale * EaseOutCubic(t);
-                t += Time.deltaTime * hops / hoppingTime;
+                t += Time.deltaTime * time;
                 float x = transform.position.x;
                 float y = parent.transform.position.y +
                     Mathf.Abs(Mathf.Sin(t * Mathf.PI)) * hoppingHeight;
@@ -427,6 +508,11 @@ public class PlayerTitleMotioner : MonoBehaviour
         isMotionStarted = false;
     }
 
+    /// <summary>
+    /// 自作のイージング関数。二次関数を折り返しただけのグラフ。
+    /// </summary>
+    /// <param name="t">グラフに対する時間軸。戻り値は0.5で1、1.0で0に戻る。</param>
+    /// <returns></returns>
     private float EaseInReturn(float t)
     {
         float constValue1 = 2 * t;
@@ -436,6 +522,12 @@ public class PlayerTitleMotioner : MonoBehaviour
             constValue1 * constValue1 :
             constValue2 * constValue2;
     }
+
+    /// <summary>
+    /// イージング関数。なだらかに上昇し後半にかけて急激に上昇するグラフ。
+    /// </summary>
+    /// <param name="t">グラフに対する時間軸。1.0で1が返ってくる。</param>
+    /// <returns></returns>
     private float EaseInExpo(float t)
     {
         return t == 0 ?
@@ -445,11 +537,22 @@ public class PlayerTitleMotioner : MonoBehaviour
             1.0f;
     }
 
+    /// <summary>
+    /// イージング関数。急激に上昇し後半にかけてなだらかになっていくグラフ。
+    /// </summary>
+    /// <param name="t">グラフに対する時間軸。1.0で1が返ってくる。</param>
+    /// <returns></returns>
     private float EaseOutCubic(float t)
     {
         float c = 1 - t;
-        return 1 - c * c * c;
+        return t < 1.0 ? 1 - c * c * c : 1.0f;
     }
+
+    /// <summary>
+    /// イージング関数。上下に飛び出すＳ字みたいなグラフ。
+    /// </summary>
+    /// <param name="t">グラフに対する時間軸。1.0で1が返ってくる。</param>
+    /// <returns></returns>
     private float EaseInOutBack(float t)
     {
         const float c1 = 1.70158f;
