4386f53097d6920587ee86295aa260241cc0fa57
kumagai <sdt22csc0005@edu.sca.ac.jp>
2024-12-14 18:49:52 +0900

    壁とプレイヤーの距離を測り、一定以下ならコライダーのアクティブ状態をオフにする機能を追加

---
+++ Assets/Scripts/Map/WallManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    Collider2D collider;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        collider=this.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (player.transform.position - this.transform.position).magnitude;
+        collider.enabled = (dist < 5);
+    }
+}
