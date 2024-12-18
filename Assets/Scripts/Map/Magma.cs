e3e1052986842683bf84c65d7271782dd338292f
hata <sdt22csc0012@edu.sca.ac..jp>
2024-12-18 19:48:32 +0900

    magma途中

--- Assets/Scripts/Map/Magma.cs
+++ Assets/Scripts/Map/Magma.cs
@@ -6,13 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class Magma : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float defSpeed;
+    [SerializeField] private float speed;
+    [SerializeField] private string playerName;
+    private GameObject player;
+    private Vector3 playerPos;
+    private const int mapHeight = 13;
+    private float oreEffectSpeedRate;
+    private float timer;
+
+    private void Start()
+    {
+        player = GameObject.Find(playerName);
+        playerPos = player.transform.position;
+        speed = defSpeed;
+        oreEffectSpeedRate = 1.0f;
+        timer = 0.0f;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, speed, 0)*Time.deltaTime;
-        
+        Vector3 spd = Vector3.zero;
+        float deff=Mathf.Abs(transform.position.y - playerPos.y);
+        if (deff >= mapHeight)
+        {
+            speed = minSpeed;
+        }
+        //spd.y = 1.0f / speed * oreEffectSpeedRate<=maxSpeed?
+
+        if (timer > 30.0f) transform.position += spd;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
