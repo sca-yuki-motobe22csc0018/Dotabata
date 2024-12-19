387b964cd8372945fa7d270e39482b92d6067afb
Yuki <sdt22csc0018@edu.sca.ac.jp>
2024-12-19 16:22:28 +0900

    プレイヤー動き

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -15,16 +15,27 @@ public class PlayerMove : MonoBehaviour
     public float bouncinessUp;
     public float friction;
     public GameObject PlayerSkin;
+    private float skinSize=0.5f;
     [SerializeField] Rigidbody2D rb = new Rigidbody2D();
+
     public float PowerMaxTime;
-    private float PowerCoolTime;
+    private float PowerCoolTimeRight;
+    float PowerTimerRight;
+    private float PowerCoolTimeLeft;
+    float PowerTimerLeft;
+
     public float PowerCoolTimeNeutral;
     public float PowerCoolTimeDown;
     public float PowerCoolTimeUp;
-    float PowerTimer;
-    public Image gaugeImage; // ゲージに使用するImageコンポーネント
-    [Range(0, 1)] public float fillAmount = 1.0f; // 塗りつぶし率
-    bool move;
+
+    
+    //public Image gaugeImage; // ゲージに使用するImageコンポーネント
+    public Image gaugeImageRight; // ゲージに使用するImageコンポーネント
+    public Image gaugeImageLeft; // ゲージに使用するImageコンポーネント
+    [Range(0, 1)] public float fillAmountRight = 1.0f; // 塗りつぶし率
+    [Range(0, 1)] public float fillAmountLeft = 1.0f; // 塗りつぶし率
+    bool moveRight;
+    bool moveLeft;
     [SerializeField] private Color fullHealthColor = Color.white;   // HP最大時の色
     [SerializeField] private Color lowHealthColor = Color.red;     // HP最小時の色
     [SerializeField] private float forceMultiplier = 10f; // 力の大きさを調整
@@ -67,12 +78,17 @@ public class PlayerMove : MonoBehaviour
         pm = gameManger.GetComponent<PlayerManager>();
         pm.AddFunction(PlayerUpdate);
         cam= playerCamera.GetComponent<Camera>();
-        move = false;
-        PowerTimer = 0;
+        moveRight = false;
+        moveLeft = false;
+        //PowerTimer = 0;
+        PowerTimerRight = 0;
+        PowerTimerLeft = 0;
         rb = GetComponent<Rigidbody2D>();
         //physicsMaterial.bounciness = bouncinessNeutral;
         dynamicMaterial = new PhysicsMaterial2D();
-        PowerCoolTime = PowerCoolTimeNeutral;
+        //PowerCoolTime = PowerCoolTimeNeutral;
+        PowerCoolTimeRight = PowerCoolTimeNeutral;
+        PowerCoolTimeLeft = PowerCoolTimeNeutral;
         Speed = SpeedNeutral;
         camSize = camSizeNeutral;
 
@@ -87,7 +103,7 @@ public class PlayerMove : MonoBehaviour
         scoreUp = false;
         visibilityUp = false;
         lavaSpeedDown = false;
-        chargeMax = false;
+        chargeMax = true;
         destroyWithOneHit = false;
         speedUp = false;
         lavaSpeedUp = false;
@@ -100,14 +116,14 @@ public class PlayerMove : MonoBehaviour
         visibilityDown = false;
     }
 
-    // Update is called once per frame
-    private void Update()
-    {
-    }
     // 任意でゲージを設定するメソッド
-    public void SetGauge(float value)
+    public void SetGaugeRight(float value)
     {
-        fillAmount = Mathf.Clamp01(value); // 0～1にクランプ
+        fillAmountRight = Mathf.Clamp01(value); // 0～1にクランプ
+    }
+    public void SetGaugeLeft(float value)
+    {
+        fillAmountLeft = Mathf.Clamp01(value); // 0～1にクランプ
     }
 
     private void PlayerUpdate()
@@ -134,69 +150,88 @@ public class PlayerMove : MonoBehaviour
         {
             SceneManager.LoadScene("Title");
         }
-        if (gaugeImage != null)
+        if (gaugeImageRight != null && gaugeImageLeft != null)
         {
-            gaugeImage.fillAmount = fillAmount;
+            gaugeImageRight.fillAmount = fillAmountRight;
+            gaugeImageLeft.fillAmount = fillAmountLeft;
         }
-        if (Input.GetMouseButton(0) && PowerTimer < PowerMaxTime && !move)
-        {
-            PowerTimer += Time.deltaTime;
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            move = true;
-            // マウスのワールド座標を取得
-            Vector3 mouseP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouseP.z = 0; // 2DのためZ軸を0に
 
-            // クリックした方向を計算
-            Vector2 dir = (mouseP - transform.position).normalized*Speed;
+        if (Input.GetKey(KeyCode.D) && PowerTimerRight < PowerMaxTime && !moveRight)
+        {
+            PowerTimerRight += Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D)&& !moveRight)
+        {
+            Vector3 vel = rb.velocity;
+            vel.x = 0;
+            vel.y = 0;
+            rb.velocity = vel;
+            moveRight = true;
+            Vector2 dir = new Vector2(1, 1).normalized * Speed;
+            PlayerSkin.transform.localScale = new Vector3(-skinSize, skinSize, 1);
 
             // Rigidbody2Dに力を加える
-            rb.AddForce(dir * PowerTimer, ForceMode2D.Impulse);
+            rb.AddForce(dir * PowerTimerRight, ForceMode2D.Impulse);
         }
-        if (move)
+
+        if (Input.GetKey(KeyCode.A) && PowerTimerLeft < PowerMaxTime && !moveLeft)
         {
-            PowerTimer -= PowerCoolTime * Time.deltaTime;
-            gaugeImage.color = lowHealthColor;
+            PowerTimerLeft += Time.deltaTime;
         }
-        if (PowerTimer <= 0)
+        if (Input.GetKeyUp(KeyCode.A) && !moveLeft)
         {
-            PowerTimer = 0;
-            move = false;
-            gaugeImage.color = fullHealthColor;
+            Vector3 vel = rb.velocity;
+            vel.x = 0;
+            vel.y = 0;
+            rb.velocity = vel;
+            moveLeft = true;
+            Vector2 dir = new Vector2(-1,1).normalized * Speed;
+            PlayerSkin.transform.localScale = new Vector3(skinSize, skinSize, 1);
+
+            // Rigidbody2Dに力を加える
+            rb.AddForce(dir * PowerTimerLeft, ForceMode2D.Impulse);
         }
-        SetGauge(PowerTimer / PowerMaxTime);
-
-        // マウスのスクリーン座標を取得
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0f; // 2DなのでZ座標は無視
-
-        // オブジェクトの位置からマウスの位置までのベクトルを計算
-        Vector2 direction = mousePosition - transform.position;
-
-        // ベクトルの角度を取得して回転させる
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        if (!move&&PlayerSkin!=null)
+        if (moveLeft && moveRight)
         {
-            PlayerSkin.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-            if (PlayerSkin.transform.rotation.z > 0.7f || PlayerSkin.transform.rotation.z < -0.7f)
-            {
-                PlayerSkin.transform.localScale = new Vector3(-0.5f, -0.5f, 1);
-            }
-            else
-            {
-                PlayerSkin.transform.localScale = new Vector3(-0.5f, 0.5f, 1);
-            }
+            Vector3 vel = rb.velocity;
+            vel.x = 0;
+            rb.velocity = vel;
         }
+        if (moveRight)
+        {
+            PowerTimerRight -= PowerCoolTimeRight * Time.deltaTime;
+            gaugeImageRight.color = lowHealthColor;
+        }
+        if (PowerTimerRight <= 0)
+        {
+            PowerTimerRight = 0;
+            moveRight = false;
+            gaugeImageRight.color = fullHealthColor;
+        }
+        SetGaugeRight(PowerTimerRight / PowerMaxTime);
+
+        if (moveLeft)
+        {
+            PowerTimerLeft -= PowerCoolTimeLeft * Time.deltaTime;
+            gaugeImageLeft.color = lowHealthColor;
+        }
+        if (PowerTimerLeft <= 0)
+        {
+            PowerTimerLeft = 0;
+            moveLeft = false;
+            gaugeImageLeft.color = fullHealthColor;
+        }
+        SetGaugeLeft(PowerTimerLeft / PowerMaxTime);
+
         //鉱石関連
         //Plus
         if (coolTimeDown)
         {
-            if (PowerCoolTime != PowerCoolTimeDown)
+            if (PowerCoolTimeRight != PowerCoolTimeDown)
             {
-                PowerCoolTime = PowerCoolTimeDown;
+                PowerCoolTimeRight = PowerCoolTimeDown;
+                PowerCoolTimeLeft = PowerCoolTimeDown;
             }
         }
         if (knockbackDown)
@@ -230,10 +265,15 @@ public class PlayerMove : MonoBehaviour
         //Neutral
         if (chargeMax)
         {
-            if (Input.GetMouseButton(0) && PowerTimer < PowerMaxTime && !move)
+            if (Input.GetKey(KeyCode.D) && PowerTimerRight < PowerMaxTime && !moveRight)
             {
-                PowerTimer = PowerMaxTime;
+                PowerTimerRight = PowerMaxTime;
             }
+            if (Input.GetKey(KeyCode.A) && PowerTimerLeft < PowerMaxTime && !moveLeft)
+            {
+                PowerTimerLeft = PowerMaxTime;
+            }
+
         }
         if (destroyWithOneHit)
         {
@@ -253,9 +293,10 @@ public class PlayerMove : MonoBehaviour
         }
         if (coolTimeUp)
         {
-            if (PowerCoolTime != PowerCoolTimeUp)
+            if (PowerCoolTimeRight != PowerCoolTimeUp)
             {
-                PowerCoolTime = PowerCoolTimeUp;
+                PowerCoolTimeRight = PowerCoolTimeUp;
+                PowerCoolTimeLeft = PowerCoolTimeUp;
             }
         }
         if (scoreDown)
@@ -298,9 +339,10 @@ public class PlayerMove : MonoBehaviour
         //戻す処理
         if (!coolTimeDown&&!coolTimeUp)
         {
-            if (PowerCoolTime != PowerCoolTimeNeutral)
+            if (PowerCoolTimeRight != PowerCoolTimeNeutral)
             {
-                PowerCoolTime = PowerCoolTimeNeutral;
+                PowerCoolTimeRight = PowerCoolTimeNeutral;
+                PowerCoolTimeLeft = PowerCoolTimeNeutral;
             }
         }
         if (!knockbackUp && !knockbackDown)
