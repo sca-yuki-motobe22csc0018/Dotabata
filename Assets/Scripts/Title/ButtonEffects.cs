ab8b2f6c99ff6ac9532f50466256c5cedbee7e8f
hata <sdt22csc0012@edu.sca.ac.jp>
2025-01-09 19:46:05 +0900

    オン／オフカーソル時のボタン拡縮

--- Assets/Scripts/Title/ButtonEffects.cs
+++ Assets/Scripts/Title/ButtonEffects.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonEffects : MonoBehaviour
@@ -12,44 +14,120 @@ public class ButtonEffects : MonoBehaviour
         OPTION,
         EXIT
     }
-
-    [SerializeField] private GameObject buttonObject;
-    private Image[] buttonImage;
+    enum ButtonState
+    {
+        OFF_CURSOR,
+        ON_CURSOR,
+    }
+    private Image buttonBackGroundImage;
     [SerializeField] private GameObject[] buttonDecoration;
     [SerializeField] private Sprite[] buttonSprites;
-    [SerializeField] private Sprite[] buttonEffectSprites;
-    [SerializeField] private ButtonMode mode;
-    private bool isMotionEnd = false;
-    
+    [SerializeField] private GameObject offContents;
+    [SerializeField] private GameObject onContents;
+    private ButtonMode mode;
+    private ButtonState state;
+
+    private Vector3 onCursorScale;
+    private Vector3 offCursorScale;
     // Start is called before the first frame update
     void Start()
     {
-        buttonImage[0] = transform.GetChild(0).gameObject.GetComponent<Image>();
-        buttonImage[1] = transform.GetChild(1).gameObject.GetComponent<Image>();
+        switch (gameObject.name)
+        {
+            case "Start":
+                mode = ButtonMode.START;
+                break;
+
+
+            case "Library":
+                mode = ButtonMode.LIBRARY;
+                break;
+
+            case "Option":
+                mode = ButtonMode.OPTION;
+                break;
+
+            case "Exit":
+                mode = ButtonMode.EXIT;
+                break;
+
+        }
+        buttonBackGroundImage = transform.GetChild(0).gameObject.GetComponent<Image>();
+        buttonBackGroundImage.sprite = buttonSprites[0];
         buttonDecoration[0].SetActive(false);
         buttonDecoration[1].SetActive(false);
+        onContents.SetActive(false);
+        offContents.SetActive(true);
+        state = ButtonState.OFF_CURSOR;
+
+        offCursorScale = Vector3.one;
+        onCursorScale = Vector3.one * 1.25f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(state == ButtonState.OFF_CURSOR)
+        {
+            buttonBackGroundImage.sprite = buttonSprites[0];
+            buttonDecoration[0].SetActive(false);
+            buttonDecoration[1].SetActive(false);
+            onContents.SetActive(false);
+            offContents.SetActive(true);
+
+            if (transform.localScale.x > offCursorScale.x)
+                transform.localScale -= onCursorScale * Time.deltaTime * 5.0f;
+            else if(transform.localScale.x < offCursorScale.x)
+                transform.localScale = offCursorScale;
+        }
+        else
+        {
+            buttonBackGroundImage.sprite = buttonSprites[1];
+            buttonDecoration[0].SetActive(true);
+            buttonDecoration[1].SetActive(true);
+            onContents.SetActive(true);
+            offContents.SetActive(false);
+
+            if (transform.localScale.x < onCursorScale.x)
+                transform.localScale += onCursorScale * Time.deltaTime * 5.0f;
+            else if (transform.localScale.x > onCursorScale.x)
+                transform.localScale = onCursorScale;
+        }
     }
 
-    private IEnumerator EnterStartButton()
+    public void OnClick()
     {
-        buttonImage[0].sprite = buttonSprites[1];
-        buttonImage[1].sprite = buttonEffectSprites[1];
-        buttonDecoration[0].SetActive(true);
-        buttonDecoration[1].SetActive(true);
-
-        bool isEnd = false;
-        float t = 0.0f;
-
-        while (!isEnd)
+        string sceneName = null;
+        switch (mode)
         {
+            case ButtonMode.START:
+                sceneName = "MapTest";
+                break;
 
+            case ButtonMode.LIBRARY:
+                sceneName = "Library";
+                break;
+
+                case ButtonMode.OPTION:
+                sceneName = "Option";
+                break;
+
+                case ButtonMode.EXIT:
+                Application.Quit();
+                break;
         }
-        yield return null;
+
+        if(sceneName != null)
+            SceneManager.LoadScene(sceneName);
+    }
+
+   public void PointerEnter()
+    {
+        state = ButtonState.ON_CURSOR;
+    }
+
+    public void PointerExit()
+    {
+        state= ButtonState.OFF_CURSOR;
     }
 }
