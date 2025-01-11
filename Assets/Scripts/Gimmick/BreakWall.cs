f58d4421277fe8ffce9d95d362047f4a1b1e874c
kato_kenji <sdt22csc0002@edu.sca.ac.jp>
2025-01-11 17:51:38 +0900

    aaa

--- Assets/Scripts/Gimmick/BreakWall.cs
+++ Assets/Scripts/Gimmick/BreakWall.cs
@@ -7,28 +7,35 @@ public class BreakWall : MonoBehaviour
     [SerializeField]Collider2D col;
     SpriteRenderer spriteRenderer;
 
+    GameObject player;
+    Rigidbody2D rb;
+    float playerPower;
+
 
     void Start()
     {
         col = this.gameObject.GetComponent<Collider2D>();
         spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+
+        player = GameObject.Find("Player");
+        rb = player.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.A))
-        {
-            col.enabled = false;
-            spriteRenderer.color = new Color(0, 0, 0, 0);
-        }
+        playerPower = Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y);
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            col.enabled = false;
-            spriteRenderer.color = new Color(0, 0, 0, 0);
+            if(playerPower >= 3.0f)
+            {
+                col.enabled = false;
+                spriteRenderer.color = new Color(0, 0, 0, 0);
+                Destroy(this.gameObject);
+            }
         }
     }
 }
